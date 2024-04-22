3fd022230b6bac76a41bf36e3256630aeb50b4c6
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2024-04-23 00:43:16 +0300

    feat: Insert Sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,35 +1,46 @@
 "use client"
 
-import { Menu } from "lucide-react"
 import { Poppins } from "next/font/google"
 import Link from "next/link"
+import Image from "next/image"
+import profilePic from "@/public/RU_s.png"
 
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
-import { ModeToggle } from "./mode-toggle";
+import { ModeToggle } from "@/components/mode-toggle";
+import { MobileSidebar } from "@/components/mobile-sidebar";
 
 const font = Poppins({
     weight: "600",
-    subsets: ["latin"]
+    subsets: ["latin"],
 });
 
 export const Navbar = () => {
     return (
-        <div className="flexed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
-            <div className="flex items-center">
-                <Menu className="block md:hidden"/>
-                <Link href={`/`}>
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
+            <div className="w-full flex items-center justify-between border border-white">
+                <MobileSidebar />
+                <Link href={`/`} className="flex justify-between  items-end">
+                    <Image
+                        src={profilePic}
+                        alt="Picture of the author"
+                        width={60}
+                        height={60}
+                    />
                     <h1 className={cn(
-                        "hidden md:block text-xl md:text-3xl font-bold text-primary",
+                        "hidden md:block text-xl md:text-base font-bold text-primary",
                         font.className
                     )}>
                         abshaga.5
                     </h1>
                 </Link>
-            </div>
-            <div className="flex items-center gap-x-3">
-                <ModeToggle />
-                <UserButton />
+                <div className="flex items-center gap-x-3">
+                    <div>
+                        <h3 className="font-bold">Student</h3>
+                    </div>
+                    <ModeToggle />
+                    <UserButton />
+                </div>
             </div>
         </div>
     )
